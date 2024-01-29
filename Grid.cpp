92dcbdbe171157e49b1be6fcb5fb1b696e448f4f
sahil-tgs <sahiltgs@gmail.com>
2024-01-29 21:51:53 +0530

    grid-module-added

---
+++ Grid.cpp
@@ -0,0 +1,47 @@
+#include "Grid.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+Grid::Grid(int rows, int cols) : rows_(rows), cols_(cols), data_(rows, std::vector<bool>(cols, false)) {
+    // Seed the random number generator
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+}
+
+void Grid::initializeRandom() {
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            data_[i][j] = std::rand() % 2 == 0;
+        }
+    }
+}
+
+void Grid::initializeFromUserInput() {
+    // Implement user input for initializing the grid
+    // You can use std::cin to get input from the user
+}
+
+void Grid::print() {
+    system("clear");  // For Linux/Mac. Use "cls" for Windows.
+
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            std::cout << (data_[i][j] ? '#' : ' ');
+        }
+        std::cout << std::endl;
+    }
+}
+
+bool Grid::getCell(int x, int y) const {
+    if (x >= 0 && x < rows_ && y >= 0 && y < cols_) {
+        return data_[x][y];
+    }
+    return false; // Consider out-of-bounds as dead cell
+}
+
+void Grid::setCell(int x, int y, bool value) {
+    if (x >= 0 && x < rows_ && y >= 0 && y < cols_) {
+        data_[x][y] = value;
+    }
+}
+
---
+++ Grid.h
@@ -0,0 +1,23 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <vector>
+
+class Grid {
+public:
+    Grid(int rows, int cols);
+    void initializeRandom();
+    void initializeFromUserInput();
+    void print();
+    int getRows() const { return rows_; }
+    int getCols() const { return cols_; }
+    bool getCell(int x, int y) const;
+    void setCell(int x, int y, bool value);
+
+private:
+    int rows_;
+    int cols_;
+    std::vector<std::vector<bool>> data_;
+};
+
+#endif
